9c72598b22c6a3fda62b21b8b876d454dd8438b1
AlexandreIorio <alexandre.iorio@heig-vd.ch>
2023-11-24 19:48:54 +0100

    WORK: Script and  Relation modeling

--- RelationalModeling/database.sql
+++ RelationalModeling/database.sql
@@ -1,11 +1,15 @@
 SET search_path TO membershipmanager;
 
+
+
 CREATE TABLE canton (
 abbreviation varchar(2),
 name varchar(50),
 
 PRIMARY KEY (abbreviation)
 );
+);
+
 
 Create TABLE city (
 npa int,
@@ -13,7 +17,7 @@ name varchar(50),
 canton_name varchar(50),
 
 PRIMARY KEY (npa),
-FOREIGN KEY (canton_name) REFERENCES Canton(canton_name)
+FOREIGN KEY (canton_name) REFERENCES Canton(abbreviation)
 );
 
 CREATE TABLE structure
@@ -31,23 +35,23 @@ CREATE TABLE franchise (
     structure_name varchar(50),
     npa int,
     address varchar(50),
-    
+
 PRIMARY KEY (id),
-FOREIGN KEY (structure_id) REFERENCES structure(name),
+FOREIGN KEY (structure_name) REFERENCES structure(name),
 FOREIGN KEY (npa) REFERENCES city(npa)
 );
-
-)
 
 CREATE TABLE person (
 no_avs int,
 last_name varchar(50),
 address varchar(50),
+npa int,
 phone varchar(15), -- does not store the first 0 or the country code
 mobile varchar(15), -- does not store the first 0 or the country code
 email varchar(50),
 
-PRIMARY KEY (no_avs)
+PRIMARY KEY (no_avs),
+FOREIGN KEY (npa) REFERENCES city(npa)
 );
 
 CREATE TABLE person_name (
@@ -58,16 +62,111 @@ npa int,
 name varchar(50),
 
 PRIMARY KEY (no_avs, name_order),
-FOREIGN KEY (no_avs) REFERENCES person(no_avs)
+FOREIGN KEY (no_avs) REFERENCES person(no_avs),
 FOREIGN KEY (npa) REFERENCES city(npa)
 );
+
+CREATE TABLE Employe (
+    no_avs int,
+    franchise_id int,
+    salary int, --cents
+    rate int, --percents
+
+    PRIMARY KEY (no_avs),
+    FOREIGN KEY (no_avs) references person(no_avs),
+    FOREIGN KEY (franchise_id) references franchise(id)
+);
+
+CREATE TABLE member(
+    no_avs int,
+    structure_name varchar(50),
+    subscription_date date,
+
+    PRIMARY KEY (no_avs),
+    FOREIGN KEY (no_avs) REFERENCES person(no_avs),
+    FOREIGN KEY (structure_name) REFERENCES structure(name)
+);
+CREATE TABLE memberaccount (
+    id int, -- owner id
+    credit int, --cents
+    debit int, --cents
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES member(no_avs)
+);
+
+CREATE TABLE product
+(
+code int,
+price int, --cents
+
+name varchar(50),
+
+PRIMARY KEY(code)
+);
+
+CREATE TABLE consumption(
+    id int,
+    account_id int,
+    code int,
+    date date,
+
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (account_id) REFERENCES memberAccount(id),
+    FOREIGN KEY (code) REFERENCES product(code)
+);
+
+CREATE TABLE users(
+    id int,
+    username varchar(50),
+    password_hash varchar(50),
+    salt varchar(50),
+    employe_no_avs int NOT NULL UNIQUE,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (employe_no_avs) REFERENCES employe(no_avs)
+);
+
+CREATE TABLE role(
+    id int,
+    name varchar(50),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE user_role(
+    user_id int,
+    role_id int,
+
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+
+CREATE TABLE permission(
+    id int,
+    name varchar(50),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role_permission(
+    role_id int,
+    permission_id int,
+
+    PRIMARY KEY (role_id, permission_id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (permission_id) REFERENCES permission(id)
+);
+
+
 
 CREATE TABLE documents(
 id int,
 content text,
 franchise_id int,
 
-PRIMARY KEY (id)
+PRIMARY KEY (id),
 FOREIGN KEY (franchise_id) REFERENCES franchise(id)
 );
 
@@ -90,10 +189,13 @@ FOREIGN KEY (person_id) REFERENCES person(no_avs)
 CREATE TABLE paiement
 (
 id int,
+    account_id int,
 amount int,
 date date,
 
-PRIMARY KEY (id)
+PRIMARY KEY (id),
+FOREIGN KEY (account_id) REFERENCES memberAccount(id)
+
 );
 
 CREATE TABLE cashier
@@ -113,15 +215,6 @@ PRIMARY KEY (id),
 FOREIGN KEY (id) REFERENCES paiement(id)
 );
 
-CREATE TABLE product
-(
-code int,
-price int, --cents
-
-name varchar(50),
-
-PRIMARY KEY(code)
-);
 
 CREATE TABLE consumable
 (
@@ -129,31 +222,47 @@ code int,
 franchise_id int,
 
 PRIMARY KEY (code),
-FOREIGN KEY(code) REFERENCES product(code)
+FOREIGN KEY(code) REFERENCES product(code),
 FOREIGN KEY (franchise_id) REFERENCES franchise(id)
 );
 
 CREATE TABLE entry (
     code int,
 subscription_date date,
-structure_id int,
+strucure_name varchar(50),
 
 PRIMARY KEY (code),
-FOREIGN KEY (code) REFERENCES product(code)
-FOREIGN KEY (structure_id) REFERENCES structure(name)
+FOREIGN KEY (code) REFERENCES product(code),
+FOREIGN KEY (strucure_name) REFERENCES structure(name)
 );
 
 CREATE TABLE uniqueEntry
 (
+    code int,
+
+    PRIMARY KEY (code),
+    FOREIGN KEY (code) references entry(code)
 
 );
 
 CREATE TABLE multipleEntry
 (
+    code int,
+    num_of_entry int,
+    entries_recorded int,
+    validity int, --month
+
+    PRIMARY KEY (code),
+    FOREIGN KEY (code) references entry(code)
 
 );
 
 CREATE TABLE subscription
 (
+    code int,
+    duration int, --month
+
+    PRIMARY KEY (code),
+    FOREIGN KEY (code) references entry(code)
 
 );
