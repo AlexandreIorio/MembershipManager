d1d69ca6ea7f8a624291d5ba92b96e798ffffa0c
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2023-12-22 22:02:35 +0100

    ADD: Franchise, Structure and Member

---
+++ src/MembershipManager/MembershipManager/DataModel/Company/Structure.cs
@@ -0,0 +1,21 @@
+﻿using MembershipManager.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipManager.DataModel.Company
+{
+    public class Structure
+    {
+        [DbAttribute("name")]
+        public string Name { get; set; }
+
+        [DbAttribute("head_office_address")]
+        public string? HeadOfficeAddress { get; set; }
+
+        [DbRelation("city_id")]
+        City? City { get; set; }
+    }
+}
--- src/MembershipManager/MembershipManager/DataModel/Person/Member.cs
+++ src/MembershipManager/MembershipManager/DataModel/Person/Member.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MembershipManager.Engine;
+using Npgsql;
+using NpgsqlTypes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,22 @@ namespace MembershipManager.DataModel.Person
 {
     public class Member : Person
     {
+        [DbAttribute("strucuture_name")]
+        public string StructureName { get; set; }
+
+        [DbAttribute("subscription_date")]
+        public DateTime SubscriptionDate { get; set; }
+
+
+        public static Person? getMember(object noAvs)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.CommandText = $"SELECT * FROM get_member WHERE no_avs = @value1";
+            NpgsqlParameter param = new NpgsqlParameter("@value1", NpgsqlDbType.Char, 13) { Value = noAvs };
+            cmd.Parameters.Add(param);
+
+            return DbManager.Db?.Receive<Member>(cmd).First() ?? null;
+        }
+
     }
 }
