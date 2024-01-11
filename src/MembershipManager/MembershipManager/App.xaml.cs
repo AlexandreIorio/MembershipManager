9ba1e933291335df6e2dfa5291336f7fc4bb7df0
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-01-11 21:59:36 +0100

    ADD: Splashscreen and configure app.xaml

--- src/MembershipManager/MembershipManager/App.xaml.cs
+++ src/MembershipManager/MembershipManager/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Windows;
 
 namespace MembershipManager
@@ -9,6 +10,25 @@ namespace MembershipManager
     /// </summary>
     public partial class App : Application
     {
+        private const int splashScreenDelay = 2000;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SplashScreen splashScreen = new SplashScreen();
+            splashScreen.Show();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            base.OnStartup(e);
+            timer.Stop();
+            int remainingTimeToShowSplashScreen = splashScreenDelay - (int)timer.ElapsedMilliseconds;
+            if (remainingTimeToShowSplashScreen > 0)
+            {
+                System.Threading.Thread.Sleep(remainingTimeToShowSplashScreen);
+            }
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            splashScreen.Close();
+        }
     }
 
 }
