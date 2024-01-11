fb128fb25bc9bf4f2f6493f61cefae407e481947
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-01-11 19:45:33 +0100

    WORK: Entry ok

--- src/MembershipManager/MembershipManager/View/Utils/UserControls/Entry.xaml.cs
+++ src/MembershipManager/MembershipManager/View/Utils/UserControls/Entry.xaml.cs
@@ -13,25 +13,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MembershipManager.Utils.UserControls
+namespace MembershipManager.View.Utils.UserControls
 {
     /// <summary>
     /// Logique d'interaction pour EntryH.xaml
     /// </summary>
-    public partial class EntryH : UserControl
+    public partial class Entry : UserControl
     {
         public TextBox TextBox { get => Tb; }
+        public HorizontalAlignment TextBoxAlignment { get => Tb.HorizontalAlignment; set => Tb.HorizontalAlignment = value; }
+
+        public Label Label { get => Lab; }
+
+        public Orientation Orientation { get => Sp.Orientation; set => Sp.Orientation = value; }
+
+        public string Text { get => Tb.Text; set => Tb.Text = value; }
         public string Title { get => Lab.Content.ToString(); set => Lab.Content = value; }
 
-        public double LabelWidth { get => Lab.Width;  set => Lab.Width = value; }
-        public double TextBoxWidth { get => Tb.Width;  set => Tb.Width = value; }
+        public double LabelWidth { get => Lab.Width; set => Lab.Width = value; }
+        public double TextBoxWidth { get => Tb.Width; set => Tb.Width = value; }
         public HorizontalAlignment LabelAlignment { get => Lab.HorizontalAlignment; set => Lab.HorizontalAlignment = value; }
-        public EntryH()
+        public Entry()
         {
             InitializeComponent();
             //default values
-            LabelWidth = 80;
-            TextBoxWidth = 150;
+            Orientation = Orientation.Horizontal;
         }
     }
 }
