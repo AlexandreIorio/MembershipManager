73e506845268a612d417a161ea122899697b5064
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-01-16 21:28:04 +0100

    EDIT: ITransaction

--- src/MembershipManager/MembershipManager/DataModel/Financial/ITransaction.cs
+++ src/MembershipManager/MembershipManager/DataModel/Financial/ITransaction.cs
@@ -8,9 +8,14 @@ namespace MembershipManager.DataModel.Financial
 {
     internal interface ITransaction
     {
+        enum TransactionType
+        {
+            DEBIT, CREDIT
+        }
         Account? Account { get; set; }
         DateTime? Date { get; set; }
         string? Description { get; set; }
         int? Amount { get; set; }
+        TransactionType? Type { get; set; }
     }
 }
