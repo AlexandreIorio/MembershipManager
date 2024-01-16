ba280d0d833030179f2ae3bb612603296839e969
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-01-16 22:37:53 +0100

    WORK: Account detail window

--- src/MembershipManager/MembershipManager/View/Financial/AccountDetailWindows.xaml.cs
+++ src/MembershipManager/MembershipManager/View/Financial/AccountDetailWindows.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MembershipManager.DataModel.Financial;
+using MembershipManager.DataModel.People;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +22,34 @@ namespace MembershipManager.View.Financial
     public partial class AccountDetailWindows : Window
     {
         public List<ITransaction> Transactions { get; set; } = new List<ITransaction>();
-        public AccountDetailWindows()
+        public Member Member { get; private set; }
+
+        public string MemberName
         {
+            get
+            {
+                return Member.FirstName + " " + Member.LastName;
+            }
+        }   
+
+        public int Balance
+        {
+            get
+            {
+                int balance = 200;
+                foreach (ITransaction t in Transactions)
+                {
+                    balance += t.GetAmount();
+                }
+                return balance;
+            }
+        }
+
+        public AccountDetailWindows(Member member)
+        {
+            Member = member;
             InitializeComponent();
+            DataContext = this;
         }
     }
 }
