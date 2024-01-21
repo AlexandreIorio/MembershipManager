f1d6e894ad77c6f8ecc24000480afb98f9f9252c
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-01-21 17:41:51 +0100

    EDIT: Sql files

--- src/MembershipManager/MembershipManager/Resources/Sql/database.sql
+++ src/MembershipManager/MembershipManager/Resources/Sql/database.sql
@@ -141,8 +141,6 @@ CREATE TABLE settings
     id int,
     payment_terms int,
     payment_cash bool, -- default value for payement type
-    entry_price int, --cents
-
 
     PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES franchise(id)
